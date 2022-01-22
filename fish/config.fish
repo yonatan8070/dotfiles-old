8472176117443b8d81956954b65cb5d0e2d7f553
Yonatan Avhar <yonatan3058@outlook.com>
2022-01-22 23:07:46 +0200

    Fish additions

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,11 @@ set -x _JAVA_AWT_WM_NONREPARENTING 1
 # QT theme
 set -x QT_QPA_PLATFORMTHEME qt5ct
 
+# Make bat colorize man pages
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
 set fish_greeting (set_color C41202 --bold)">"(set_color normal) Welcome, operator. Using (fish --version | sed 's/, version//'). Good luck.
 
 thefuck --alias | source
 
-# Start compositor only on tty1
-if test (tty) = /dev/tty1
-    sway
-end
+alias miniterm="python3 -m serial.tools.miniterm -e"
